6f0bcfe224794cbcca85a035ac57aa66022405ea
tletz24 <trentonsletz@icloud.com>
2017-11-19 16:05:02 -0500

    Version 1.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,24 +17,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // getting loader and a pane for the first scene.
-        // loader will then give a possibility to get related controller
         FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("LoginWindow.fxml"));
         Parent firstPane = firstPaneLoader.load();
         Scene firstScene = new Scene(firstPane, 400, 251);
 
-        // getting loader and a pane for the second scene
         FXMLLoader secondPageLoader = new FXMLLoader(getClass().getResource("FileSelectorWindow.fxml"));
         Parent secondPane = secondPageLoader.load();
         Scene secondScene = new Scene(secondPane, 300, 275);
 
-        // injecting second scene into the controller of the first scene
-        LoginWindowController firstPaneController = (LoginWindowController) firstPaneLoader.getController();
-        firstPaneController.setSecondScene(secondScene);
-
-        // injecting first scene into the controller of the second scene
         FileSelectorWindowController secondPaneController = (FileSelectorWindowController) secondPageLoader.getController();
         secondPaneController.setFirstScene(firstScene);
+
+        LoginWindowController firstPaneController = (LoginWindowController) firstPaneLoader.getController();
+        firstPaneController.setSecondScene(secondScene);
 
         primaryStage.setTitle("Switching scenes");
         primaryStage.setScene(firstScene);
