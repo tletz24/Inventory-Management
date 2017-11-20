f5e242de00b9c06f983d52f93d11438e43657223
tletz24 <trentonsletz@icloud.com>
2017-11-19 21:57:22 -0500

    Version 1.3: File Selector Window

--- src/sample/FileSelectorWindowController.java
+++ src/sample/FileSelectorWindowController.java
@@ -1,13 +1,37 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.io.File;
 
-public class FileSelectorWindowController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FileSelectorWindowController implements Initializable {
 
     public Scene firstScene;
+    public TextField fileTextField;
+
+    public ListView<String> fileViewFXML = new ListView<String>();
+
+    public void selectFile(ActionEvent event) {
+        String fileName = fileViewFXML.getSelectionModel().getSelectedItem();
+        System.out.println(fileName + " was selected");
+    }
+
+    public void createFile(ActionEvent event) {
+        String newFile = fileTextField.getText();
+        fileViewFXML.getItems().addAll(newFile);
+        fileTextField.setText("");
+    }
 
     public void setFirstScene(Scene scene) {
         firstScene = scene;
@@ -18,5 +42,20 @@ public class FileSelectorWindowController {
         primaryStage.setScene(firstScene);
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
+        try {
+            System.out.println("Retrieving Files...");
+            File directory = new File("inventoryFiles");
+            File[] fList = directory.listFiles();
+            System.out.println("Files Retrieved:");
+            for (File file : fList){
+                System.out.println("\t" + file.getName());
+                fileViewFXML.getItems().addAll(file.getName());
+            }
+        }catch(Exception e) {
+            System.out.println("Could not find the files saved");
+        }
+    }
 }
